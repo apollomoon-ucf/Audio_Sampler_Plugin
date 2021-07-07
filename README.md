e129a5c25d797852860d237ec2cb6e98f0b85680
brian_moon <apollomoon@knights.ucf.edu>
2021-07-07 16:59:42 -0400

    updated readme

--- README.md
+++ README.md
@@ -2,13 +2,28 @@
 # Vibe Music Productions - Audio Sampler Plugin
 Summer Project by Brian Moon
 
-For my second summer project, I will be building an audio plugin prototype for Vibe Music Productions, using C++ and the JUCE audio framework. Vibe is a professional recording studio and music production facility located in Ponte Vedra Beach, Florida. 
+
+# Introduction
+For one of my 2021 summer projects, I will be building an audio plugin prototype for Vibe Music Productions. Vibe is a professional recording studio and music production facility located in Ponte Vedra Beach, Florida. 
 
 Recording Studio Website: https://www.vibemusicproductions.com/
 
-(Loading VST tester in Native Instrument's Mashine 2 Software which is the target for the studio)
+The development process of the plugin will be documented in this readme.
+
+
+# Technology and Setup
+The recording studio will need the plugin to be compatible with Native Instrument's Maschine 2 running on Windows 10. C++ and the JUCE audio framework will be used to develop the plugin.
+
+(Loading VST tester in Native Instrument's Mashine 2 Software)
 ![Screenshot_Loading_Vibe_Sampler](https://user-images.githubusercontent.com/25870426/124214309-ffca7880-dabf-11eb-9eed-066e948a09a0.png)
 
 (VST success message)
 
 ![Screenshot_SuccessMessage_Vibe_Sampler](https://user-images.githubusercontent.com/25870426/124215843-d9f2a300-dac2-11eb-90a8-cd4d554ba7ac.png)
+
+
+# Design
+To visualize a rough layout of the plugin, I created a sketch using https://sketch.io/sketchpad/.
+
+(Sketch of plugin prototype)
+![Screenshot_vibe_sampler_sketch](https://user-images.githubusercontent.com/25870426/124827433-0899cf00-df44-11eb-906f-ef3fe2c57b89.png)
--- Source/PluginEditor.cpp
+++ Source/PluginEditor.cpp
@@ -6,35 +6,36 @@
   ==============================================================================
 */
 
-#include "PluginProcessor.h"
 #include "PluginEditor.h"
 
-//==============================================================================
-VibeSamplerAudioProcessorEditor::VibeSamplerAudioProcessorEditor (VibeSamplerAudioProcessor& p)
-    : AudioProcessorEditor (&p), audioProcessor (p)
-{
-    // Make sure that before the constructor has finished, you've set the
-    // editor's size to whatever you need it to be.
-    setSize (400, 300);
-}
-
-VibeSamplerAudioProcessorEditor::~VibeSamplerAudioProcessorEditor()
-{
-}
+#include "PluginProcessor.h"
 
 //==============================================================================
-void VibeSamplerAudioProcessorEditor::paint (juce::Graphics& g)
-{
-    // (Our component is opaque, so we must completely fill the background with a solid colour)
-    g.fillAll (getLookAndFeel().findColour (juce::ResizableWindow::backgroundColourId));
-
-    g.setColour (juce::Colours::white);
-    g.setFont (15.0f);
-    g.drawFittedText ("Vibe Music Productions\n Testing Maschine 2 - VST \n Brian Moon", getLocalBounds(), juce::Justification::centred, 1);
+VibeSamplerAudioProcessorEditor::VibeSamplerAudioProcessorEditor(
+    VibeSamplerAudioProcessor& p)
+    : AudioProcessorEditor(&p), audioProcessor(p) {
+  // Make sure that before the constructor has finished, you've set the
+  // editor's size to whatever you need it to be.
+  setSize(400, 300);
 }
 
-void VibeSamplerAudioProcessorEditor::resized()
-{
-    // This is generally where you'll want to lay out the positions of any
-    // subcomponents in your editor..
+VibeSamplerAudioProcessorEditor::~VibeSamplerAudioProcessorEditor() {}
+
+//==============================================================================
+void VibeSamplerAudioProcessorEditor::paint(juce::Graphics& g) {
+  // (Our component is opaque, so we must completely fill the background with a
+  // solid colour)
+  g.fillAll(
+      getLookAndFeel().findColour(juce::ResizableWindow::backgroundColourId));
+
+  g.setColour(juce::Colours::white);
+  g.setFont(15.0f);
+  g.drawFittedText(
+      "Vibe Music Productions\n Testing Maschine 2 - VST \n Brian Moon",
+      getLocalBounds(), juce::Justification::centred, 1);
+}
+
+void VibeSamplerAudioProcessorEditor::resized() {
+  // This is generally where you'll want to lay out the positions of any
+  // subcomponents in your editor..
 }
